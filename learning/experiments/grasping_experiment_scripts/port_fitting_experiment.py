115842709ae776b85f6c47fe80f2c8495caed2df
Seiji Shaw <seijis@mit.edu>
2023-04-28 16:02:33 -0400

    allow user to specify larger object ranges

--- learning/experiments/grasping_experiment_scripts/port_fitting_experiment.py
+++ learning/experiments/grasping_experiment_scripts/port_fitting_experiment.py
@@ -70,16 +70,16 @@ def port_objects(args, existing_logs_lookup, ported_logs_lookup, mode):
     # look up all the data in existing_log_json, copy files over, and store their paths in ported_logs_json
     # (for each strategy)
     if mode == 'train':
-        objs_to_port = args.train_objs_to_port
+        objs_to_port = args.num_train_objs_to_port
         geo_type = 'train_geo'
     elif mode == 'test':
-        objs_to_port = args.test_objs_to_port
+        objs_to_port = args.num_test_objs_to_port
         geo_type = 'test_geo'
     else:
         print('[Info:] %s is not a valid mode. Skipping...' % mode)
         return
 
-    for obj_ix in objs_to_port:
+    for obj_ix in range(objs_to_port):
         for strategy in existing_logs_lookup['fitting_phase'].keys():
             existing_fit_name = f'grasp_{args.existing_exp_name}_fit_{strategy}_{geo_type}_object{obj_ix}'
 
@@ -192,15 +192,15 @@ if __name__ == '__main__':
     parser.add_argument('--existing-exp-name', type=str, required=True, help='name of existing experiment to port')
     parser.add_argument('--ported-exp-name', type=str, required=True, help='name of ported experiment')
     parser.add_argument('--belief', type=str, choices=['particle', 'progressive'], required=True)
-    parser.add_argument('--train-objs-to-port', type=int, nargs='*', default=[])
-    parser.add_argument('--test-objs-to-port', type=int, nargs='*', default=[])
+    parser.add_argument('--num-train-objs-to-port', type=int, default=0)
+    parser.add_argument('--num-test-objs-to-port', type=int, default=0)
     parser.add_argument('--override-with', type=str, choices=['example'], help='override with selected fun')
     parser.add_argument('--reselect-grasps', action='store_true', default=False,
                         help='Re-select grasps for bald experiments')
 
     args = parser.parse_args()
 
-    if not args.train_objs_to_port and not args.test_objs_to_port:
+    if not args.num_train_objs_to_port and not args.num_test_objs_to_port:
         print('Must specify at least one train or test object to port over with experiment.')
     else:
         main(args)
