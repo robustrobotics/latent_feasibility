62b2d675c41596929e5353a10771d6e700670730
Seiji Shaw <seijis@mit.edu>
2023-07-10 10:39:58 -0400

    add in cherry picking for specific objects to debug

--- learning/experiments/grasping_experiment_scripts/port_fitting_experiment.py
+++ learning/experiments/grasping_experiment_scripts/port_fitting_experiment.py
@@ -8,12 +8,12 @@ import argparse
 import shutil
 import json
 from pathlib import Path
+import itertools
 import pickle
 import os
 import time
 
-
-from particle_belief import GraspingDiscreteLikelihoodParticleBelief, AmortizedGraspingDiscreteLikelihoodParticleBelief
+from particle_belief import AmortizedGraspingDiscreteLikelihoodParticleBelief
 
 LOG_ROOT = 'learning/experiments/logs'
 
@@ -72,16 +72,16 @@ def port_objects(args, existing_logs_lookup, ported_logs_lookup, mode):
     # look up all the data in existing_log_json, copy files over, and store their paths in ported_logs_json
     # (for each strategy)
     if mode == 'train':
-        objs_to_port = args.num_train_objs_to_port
+        objs_to_port = itertools.chain(range(args.num_train_objs_to_port), args.cherry_pick_train_objs_to_port)
         geo_type = 'train_geo'
     elif mode == 'test':
-        objs_to_port = args.num_test_objs_to_port
+        objs_to_port = itertools.chain(range(args.num_test_objs_to_port), args.cherry_pick_test_objs_to_port)
         geo_type = 'test_geo'
     else:
         print('[Info:] %s is not a valid mode. Skipping...' % mode)
         return
 
-    for obj_ix in range(objs_to_port):
+    for obj_ix in objs_to_port:
         for strategy in args.strategies:
             existing_fit_name = f'grasp_{args.existing_exp_name}_fit_{strategy}_{geo_type}_object{obj_ix}'
 
@@ -203,9 +203,6 @@ def port_objects(args, existing_logs_lookup, ported_logs_lookup, mode):
                         pickle.dump([np.NaN] * len(context_set_individual_grasps), handle)
 
 
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--existing-exp-name', type=str, required=True, help='name of existing experiment to port')
@@ -213,6 +210,8 @@ if __name__ == '__main__':
     parser.add_argument('--belief', type=str, choices=['particle', 'progressive'], required=True)
     parser.add_argument('--num-train-objs-to-port', type=int, default=0)
     parser.add_argument('--num-test-objs-to-port', type=int, default=0)
+    parser.add_argument('--cherry-pick-train-objs-to-port', type=int, nargs='*', default=[])
+    parser.add_argument('--cherry-pick-test-objs-to-port', type=int, nargs='*', default=[])
     parser.add_argument('--override-with', type=str, choices=['example'], help='override with selected fun')
     parser.add_argument('--reselect-grasps', action='store_true', default=False,
                         help='Re-select grasps for bald experiments')
@@ -221,7 +220,10 @@ if __name__ == '__main__':
     parser.add_argument('--strategies', nargs='+', default=['bald', 'random'])
     args = parser.parse_args()
 
-    if not args.num_train_objs_to_port and not args.num_test_objs_to_port:
+    if not (args.num_train_objs_to_port
+            or args.num_test_objs_to_port
+            or args.cherry_pick_train_objs_to_port
+            or args.cherry_pick_test_objs_to_port):
         print('Must specify at least one train or test object to port over with experiment.')
     else:
         main(args)
