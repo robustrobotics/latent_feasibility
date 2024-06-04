1dbdf45cd720099d9310a92aa2da945e61405d2c
Seiji Shaw <seiji.shaw@gmail.com>
2024-06-04 14:40:56 -0400

    commit metacomments

--- learning/data/pushing/generate_datasets_for_experiment.py
+++ learning/data/pushing/generate_datasets_for_experiment.py
@@ -2,6 +2,8 @@ import argparse
 import os
 
 
+# the equivalent function is defined in generate_dtasets_for_grasping.py
+# makes more sense just to include it in this script.
 def generate_push_data_for_obj(obj, config):
     # Call subroutines/helpers to run pybullet and get you your data!
 
@@ -17,6 +19,7 @@ def generate_push_data_for_obj(obj, config):
     return obj_data
 
 def merge_into_gnp_format(list_of_object_sets):
+    # don't imitate grasping/generate_datasets_for_experiment.py...
     # merge the object datasets together into the format specified by
     # process_and_save() and process_geometry() in create_gnp_data.py
     return {}
@@ -24,8 +27,10 @@ def merge_into_gnp_format(list_of_object_sets):
 
 def main(args):
     # load up train objects make the set
+    train_objs = [] # load in these from the object lists
+
     train_sets = []
-    for train_obj, obj_config in []:
+    for train_obj, obj_config in train_objs:
         train_sets.append(generate_push_data_for_obj(train_obj, obj_config))
 
     train_set = merge_into_gnp_format(train_sets)
@@ -37,9 +42,18 @@ def main(args):
 
 
 if __name__ == '__main__':
+    # (1)
     parser = argparse.ArgumentParser()
     # define command line arguments
-    # Consume object lists
+    # Consume object lists (filename/path)
+        # Shapenet-net-sem, box-data <-- folders that contain the object meshes.
+            # Missing (you must generate: physical properties)
+            # Action data and outcomes.
+        # object list tells  you which meshes you should use to generate physical properties + actions.
+    # Configuration params: how many pushes per object? pushing surface properties?
+    # Seed for random number generation (Seiji uses numpy's)
+    # You may need others!
+
     # parser.add_arguments...
 
     # parse command line arguments
