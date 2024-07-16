c1162c9b638ce7c5a0d16397021f23beb6c1a142
Atharv Jain <atharvjain05@gmail.com>
2024-07-16 14:45:50 -0400

    model should technically work now, unclear though

--- learning/models/push_np/push_neural_process.py
+++ learning/models/push_np/push_neural_process.py
@@ -1,16 +1,20 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from learning.models.pointnet import PointNetRegressor
 
 
 class PushNP(nn.Module):
     """
-    Input Features tells the neural network which features to use!
+    Neural network model for predicting the final position of an object after a series of pushes.
+    
+    Args:
+        input_features (list): List of input features to use for prediction.
+        d_output (int): Dimensionality of the output.
+        d_latents (int): Dimensionality of the latent space.
     """
 
-    def __init__(self, input_features, d_mesh_enc, d_latents):
+    def __init__(self, input_features, d_output, d_latents):
         super().__init__()
 
         self.input_features = input_features
@@ -18,60 +22,119 @@ class PushNP(nn.Module):
         n_mesh_features = 6  # The points, as well as the normals
         n_geom_features = 2  # Same as ^
         n_push_features = 5
+        self.n_obj_features = 5
 
         conv_1_sz = 64
         conv_2_sz = 256
         conv_3_sz = 1024
-        self.linear_layer1 = nn.Linear(conv_3_sz, 256)
-        self.linear_layer2 = nn.Linear(256, 128)
-        self.linear_layer3 = nn.Linear(128, d_latents * (d_latents + 1) / 2 + d_latents)
+        point_net_output_sz = 512
+        linear_input_sz = conv_3_sz + point_net_output_sz
+        linear_layer_1_sz = 256
+        linear_layer_2_sz = 128
+        linear_layer_3_sz = d_latents * (d_latents + 1) // 2 + d_latents
+
+        decoder_layer_1_sz = 256 
+        decoder_layer_2_sz = 1024 
+        decoder_layer_3_sz = d_output * (d_output + 1) // 2 + d_output 
+
+        self.linear_layer1 = nn.Linear(linear_input_sz, linear_layer_1_sz)
+        self.linear_layer2 = nn.Linear(linear_layer_1_sz, linear_layer_2_sz)
+        self.linear_layer3 = nn.Linear(linear_layer_2_sz, linear_layer_3_sz)
         self.point_net_encoder = PointNetRegressor(
-            n_mesh_features, d_mesh_enc, n_geometric_features=n_geom_features
-        )  # We will only use this
-        linear_input_sz = conv_3_sz + 1024
-        if "com" in input_features:
-            linear_input_sz += 5
+            n_mesh_features, point_net_output_sz, n_geometric_features=n_geom_features
+        ) 
 
         self.conv_layer_1 = nn.Conv1d(n_push_features, conv_1_sz, 1)
         self.conv_layer_2 = nn.Conv1d(conv_1_sz, conv_2_sz, 1)
         self.conv_layer_3 = nn.Conv1d(conv_2_sz, conv_3_sz, 1)
+
+        self.decoder_layer_1 = nn.Linear(d_latents + n_push_features + (self.n_obj_features if 'com' in input_features else 0), decoder_layer_1_sz)
+        self.decoder_layer_2 = nn.Linear(decoder_layer_1_sz, decoder_layer_2_sz).float()
+        self.decoder_layer_3 = nn.Linear(decoder_layer_2_sz, decoder_layer_3_sz).float()
+
         self.d_latents = d_latents
+        self.input_features = input_features
+        self.d_output = d_output
+
+        self.relu = nn.ReLU()
+        self.softplus = nn.Softplus() 
 
     def forward(self, x):
-        mu, sigma = self.get_latent_space(x)
-        return None
+        q_zs = self.get_latent_space(x)
+        (mesh_data, obj_data, push_data, n_push_data) = x
+
+        zs = torch.stack([q_z.rsample() for q_z in q_zs]).to(mesh_data.device)
+        zs = zs.to(mesh_data.device)
+        
+        latent_size = self.d_latents
+        if 'com' in self.input_features:
+            zs = torch.cat([zs, obj_data], dim=1)
+            latent_size += self.n_obj_features
+
+        zs = zs.unsqueeze(2)  # Now zs has shape [5, 10, 1]
+        zs = zs.expand(zs.shape[0], latent_size, push_data.shape[1])  # Now zs has shape [5, 10, 1]
+        zs = zs.swapaxes(1, 2)
+        # print(f"zs sz: {zs.shape}")
+        
+
+        input_vector = torch.cat([push_data, zs], dim=2)
+        input_vector = self.relu(self.decoder_layer_1(input_vector))
+        input_vector = self.relu(self.decoder_layer_2(input_vector))
+        input_vector = self.decoder_layer_3(input_vector)
+
+        # print(f"input_vector sz: {input_vector.shape}")
+        mu = input_vector[:, :, : self.d_output] 
+        sigma = input_vector[:, :, self.d_output :]
+
+        tril_indices = torch.tril_indices(
+            self.d_output, self.d_output, device=sigma.device
+        )
+
+        output = torch.zeros(
+            (input_vector.shape[0], input_vector.shape[1], self.d_output, self.d_output),
+            device=sigma.device,
+        )
+
+        for i in range(input_vector.shape[0]):
+            for k in range(input_vector.shape[1]):
+                output[i, k, tril_indices[0], tril_indices[1]] = sigma[i, k]
+                diag_indices = range(self.d_output)
+                output[i, k, diag_indices, diag_indices] = self.softplus(output[i, k, diag_indices, diag_indices])
+
+        # print(f"mu sz: {mu.shape}")
+        # print(f"output sz: {output.shape}")
+        result = [[torch.distributions.MultivariateNormal(mu, scale_tril=output[i][j]) for j in range(mu.shape[1])] for i in range(mu.shape[0])]
+        return result 
 
     def get_latent_space(self, x):
-        (mesh_data, obj_data, push_data) = x
-        print(f"object data sz: {obj_data.shape}")
-        print(f"mesh data sz: {mesh_data.shape}")
+        (mesh_data, _, _, n_push_data) = x
         mesh_vector, _ = self.point_net_encoder(mesh_data.transpose(1, 2).float())
-        print(f"push_data sz: {push_data.shape}")
-        push_vector = F.relu(self.conv_layer_1(push_data.swapaxes(1, 2).float()))
-        push_vector = F.relu(self.conv_layer_2(push_vector))
+        push_vector = self.relu(self.conv_layer_1(n_push_data.swapaxes(1, 2).float()))
+        push_vector = self.relu(self.conv_layer_2(push_vector))
         push_vector = self.conv_layer_3(push_vector)
-        push_vector = torch.max(push_vector, dim=2, keepdim=True)[0]
-        print(push_vector.shape)
+        push_vector = torch.max(push_vector, dim=2)[0]
 
-        print(f"mesh vector sz: {mesh_vector.shape}")
-        print(f"push vector sz: {push_vector.shape}")
-
-        if "com" in self.input_features:
-            input_vector = torch.cat([mesh_vector, push_vector, obj_data])
-        else:
-            input_vector = torch.cat([mesh_vector, push_vector])
-        results = F.relu(self.linear_layer1(input_vector))
-        results = F.relu(self.linear_layer2(results))
+        input_vector = torch.cat([mesh_vector, push_vector], dim=1)
+        results = self.relu(self.linear_layer1(input_vector))
+        results = self.relu(self.linear_layer2(results))
         results = self.linear_layer3(results)
 
-        mu = results[: self.d_latents]
-        variance_matrix = results[self.d_latents :]
-        tril_indices = torch.tril_indices(self.d_latents, self.d_latents)
+        mu = results[:, : self.d_latents]
+        variance_matrix = results[:, self.d_latents :]
+        tril_indices = torch.tril_indices(
+            self.d_latents, self.d_latents, device=variance_matrix.device
+        )
 
-        output = torch.zeros((self.d_latents, self.d_latents), dtype=torch.float32)
-        output[tril_indices[0], tril_indices[1]] = variance_matrix
-        for i in range(self.d_latents):
-            output[i][i] = F.softplus(output[i][i])
-        sigma = torch.matmul(output, output.T)
+        output = torch.zeros(
+            (results.shape[0], self.d_latents, self.d_latents),
+            device=variance_matrix.device,
+        )
+        for i in range(results.shape[0]):
+            output[i, tril_indices[0], tril_indices[1]] = variance_matrix[i]
+            diag_indices = range(self.d_latents)
+            output[i, diag_indices, diag_indices] = self.softplus(output[i, diag_indices, diag_indices])
 
-        return mu, sigma
+        return [
+            torch.distributions.MultivariateNormal(mu[i], scale_tril=output[i])
+            for i in range(mu.shape[0])
+        ]
--- learning/models/push_np/train_push_np.py
+++ learning/models/push_np/train_push_np.py
@@ -7,8 +7,16 @@ from torch.utils.data import DataLoader
 from learning.models.push_np.dataset import PushNPDataset, collate_fn
 from learning.models.push_np.push_neural_process import PushNP
 
+def loss_fn(predictions, targets):
+    loss = 0 
+    for i in range(targets.shape[0]):
+        for j in range(targets.shape[1]):
+            loss += predictions[i][j].log_prob(targets[i][j][:3]).sum()
+    return -loss
 
 def train(model, train_dataloader, args, n_epochs=10):
+    model.train() 
+    torch.autograd.set_detect_anomaly(True)
     if torch.cuda.is_available():
         model = model.cuda()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
@@ -23,7 +31,7 @@ def train(model, train_dataloader, args, n_epochs=10):
                 # obj_data = torch.Tensor(data["com"], data["friction"], data["mass"])
                 obj_data = torch.stack((data["mass"], data["friction"]), dim=1)
                 obj_data = torch.cat((obj_data, data["com"]), dim=1)
-                print(obj_data.shape)
+                # print(obj_data.shape)
             else:
                 obj_data = (None, None, None)
 
@@ -31,36 +39,58 @@ def train(model, train_dataloader, args, n_epochs=10):
             push_data = torch.cat((push_data, data["contact_points"]), dim=2)
 
             if torch.cuda.is_available():
-                mesh_data = mesh_data.cuda()
-                push_data = push_data.cuda()
+                mesh_data = mesh_data.cuda().float()
+                push_data = push_data.cuda().float()
                 if args.use_obj_properties:
-                    obj_data = obj_data.cuda()
+                    obj_data = obj_data.cuda().float() 
+                data["final_position"] = data["final_position"].cuda().float()
+                data["trajectory_data"] = data["trajectory_data"].cuda().float() 
 
             max_pushes = push_data.shape[1]
             n_pushes = torch.randint(low=1, high=max_pushes + 1, size=(1,))
             n_indices = torch.randperm(max_pushes)[:n_pushes]
             n_push_data = push_data[:, n_indices, :]
 
-            data = (mesh_data, obj_data, push_data)
-            n_data = (mesh_data, obj_data, n_push_data)
+            model_data = (mesh_data, obj_data, push_data, n_push_data)
 
-            test = model.forward(data)
-            break
+            predictions = model.forward(model_data)
+            loss = loss_fn(predictions, data["final_position"])
+            # print(f"Loss: {loss}")
             optimizer.zero_grad()
 
-            loss.backward()  # Need to change this
+            loss.backward()  
             optimizer.step()
 
 
 def main(args):
     data_path = os.path.join("learning", "data", "pushing", args.file_name)
     train_data = os.path.join(data_path, "train_dataset.pkl")
-    validation_data = os.path.join(data_path, "train_dataset.pkl")
-    args_path = os.path.join(data_path, "args.pkl")
-    with open(args_path, "wb") as handle:
-        pickle.dump(args, handle)
+    validation_data = os.path.join(data_path, "test_dataset.pkl")
+    instance_path = os.path.join(data_path, args.instance_name)
+    
 
-    train_dataset = PushNPDataset(train_data, args.n_samples)
+    # This is so that we don't have to create a dataset everytime. 
+    if not os.path.exists(instance_path):
+        os.makedirs(instance_path)
+        args_path = os.path.join(data_path, args.instance_name, "args.pkl")
+        with open(args_path, "wb") as handle:
+            pickle.dump(args, handle)
+
+        train_dataset = PushNPDataset(train_data, args.n_samples)
+        validation_dataset = PushNPDataset(validation_data, args.n_samples)
+        with open(os.path.join(instance_path, "train_dataset.pkl"), "wb") as handle:
+            pickle.dump(train_dataset, handle)
+        with open(os.path.join(instance_path, "validation_dataset.pkl"), "wb") as handle:
+            pickle.dump(validation_dataset, handle)
+    else:
+        print(f"Instance {args.instance_name} already exists") 
+        with open(os.path.join(instance_path, "args.pkl"), "rb") as handle:
+            args = pickle.load(handle) 
+            with open(os.path.join(instance_path, "train_dataset.pkl"), "rb") as handle: 
+                train_dataset = pickle.load(handle)
+            with open(os.path.join(instance_path, "validation_dataset.pkl"), "rb") as handle:
+                validation_dataset = pickle.load(handle)
+            
     train_dataloader = DataLoader(
         dataset=train_dataset,
         collate_fn=collate_fn,
@@ -84,8 +114,8 @@ def main(args):
         features.remove("mass")
         features.remove("com")
         features.remove("friction")
-    model = PushNP(features, 5, d_latents=5)
-    train(model, train_dataloader, args)
+    model = PushNP(features, 3, d_latents=5)
+    train(model, train_dataloader, args, args.n_epochs)
 
 
 if __name__ == "__main__":
@@ -112,6 +142,12 @@ if __name__ == "__main__":
         action="store_true",
         help="Use only if you want the actual mass,com,friction to be used in model.",
     )
+    parser.add_argument(
+        "--n-epochs", type=int, help="Number of epochs for training", required=True)
+
+    parser.add_argument("--instance-name", type=str, help="Name of the instance", required=True)
 
     args = parser.parse_args()
+
+
     main(args)
